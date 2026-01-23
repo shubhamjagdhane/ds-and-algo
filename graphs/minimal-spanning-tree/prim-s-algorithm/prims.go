f95d8913e0dec4e1042c9546ab60719b13fe9bb5
Shubham Jagdhane <shubhamjagdhane1010@gmail.com>
2026-01-23 18:09:26 +0530

    added prim's algorithm

---
+++ graphs/minimal-spanning-tree/prim-s-algorithm/prims.go
@@ -0,0 +1,58 @@
+package primsalgorithm
+
+import (
+	"container/heap"
+)
+
+type Node struct {
+	val, wt int
+}
+
+type MinHeap []Node
+
+func (mh MinHeap) Len() int           { return len(mh) }
+func (mh MinHeap) Less(i, j int) bool { return mh[i].wt < mh[j].wt }
+func (mh MinHeap) Swap(i, j int)      { mh[i], mh[j] = mh[j], mh[i] }
+func (mh *MinHeap) Push(val any)      { *mh = append(*mh, val.(Node)) }
+func (mh *MinHeap) Pop() any {
+	old := *mh
+	n := len(old)
+	temp := old[n-1]
+	*mh = old[:n-1]
+	return temp
+}
+
+// n |- int represent the total number of nodes in the graph
+// graph |- [][]int connected undirected edges where ith indicates children of ith node and edge[i] = [u, v, w] represents the edge between the nodes u and v having w edge weight.
+func minimumSpanningTree(n int, edges [][]int) int {
+	graph := make([][]Node, n)
+	for _, edge := range edges {
+		from, to, wt := edge[0], edge[1], edge[2]
+		graph[from] = append(graph[from], Node{val: to, wt: wt})
+		graph[to] = append(graph[to], Node{val: from, wt: wt})
+	}
+	minHeap := &MinHeap{}
+	heap.Init(minHeap)
+
+	// starting from the node 0
+	heap.Push(minHeap, Node{val: 0, wt: 0})
+
+	sum := 0
+	visited := make([]bool, n)
+	for minHeap.Len() > 0 {
+		node := heap.Pop(minHeap).(Node)
+		if visited[node.val] {
+			continue
+		}
+
+		visited[node.val] = true
+		sum += node.wt
+		for _, child := range graph[node.val] {
+			if !visited[child.val] {
+				heap.Push(minHeap, child)
+			}
+		}
+	}
+
+	return sum
+}
---
+++ graphs/minimal-spanning-tree/prim-s-algorithm/prims_test.go
@@ -0,0 +1,40 @@
+package primsalgorithm
+
+import "testing"
+
+func TestMinimumSpanningTree(t *testing.T) {
+	type args struct {
+		n     int
+		edges [][]int
+	}
+	testCases := []struct {
+		name     string
+		args     args
+		expected int
+	}{
+		{
+			name: "connected undirected graph with 3 vertices",
+			args: args{
+				n:     3,
+				edges: [][]int{{0, 1, 5}, {1, 2, 3}, {0, 2, 1}},
+			},
+			expected: 4,
+		},
+		{
+			name: "connected undirected graph with 7 vertices",
+			args: args{
+				n:     7,
+				edges: [][]int{{0, 1, 5}, {0, 3, 20}, {1, 2, 5}, {3, 4, 1}, {2, 3, 5}, {4, 5, 2}, {4, 6, 4}, {5, 6, 2}},
+			},
+			expected: 20,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := minimumSpanningTree(tc.args.n, tc.args.edges)
+			if tc.expected != got {
+				t.Errorf("expected %d, got %d", tc.expected, got)
+			}
+		})
+	}
+}
